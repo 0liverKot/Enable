e07839cf3bf29219cfc56b21443ef2f27b3d8df5
0liverkot <oliverkot19@gmail.com>
2025-05-26 22:12:11 +0100

    some boiler plate for user

---
+++ Server/enable/src/main/java/com/enable/enable/UserService.java
@@ -0,0 +1,15 @@
+package com.enable.enable;
+
+import org.springframework.stereotype.Service;
+
+@Service 
+public class UserService {
+    
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+   
+}
