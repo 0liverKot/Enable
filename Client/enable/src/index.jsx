384516f6ae06fb91468554b2b1742333fffbaa7c
0liverkot <oliverkot19@gmail.com>
2025-09-04 16:52:26 +0100

    removing data page, testing using postman now

--- Client/enable/src/index.jsx
+++ Client/enable/src/index.jsx
@@ -2,18 +2,12 @@ import React, { StrictMode } from 'react';
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import App from './App.jsx'
-import Data from "./Data.jsx"
 import ErrorPage from "./ErrorPage.jsx"
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App/>,
-        errorElement: <ErrorPage />,
-    },
-    {
-        path: "/data",
-        element: <Data/>,
         errorElement: <ErrorPage />,
     },
 ]);
