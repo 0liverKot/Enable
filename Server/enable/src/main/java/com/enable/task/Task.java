529e6d1d1b8f1ceae24705ee2e792ce8cfb8c818
0liverkot <oliverkot19@gmail.com>
2025-09-08 14:37:31 +0100

    fixed import in Task.java

--- Server/enable/src/main/java/com/enable/task/Task.java
+++ Server/enable/src/main/java/com/enable/task/Task.java
@@ -1,11 +1,12 @@
 package com.enable.task;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
+import com.enable.task.Frequency.FrequencyOptions;;
 
 @Entity
 @Table(name = "tasks", schema = "public")
