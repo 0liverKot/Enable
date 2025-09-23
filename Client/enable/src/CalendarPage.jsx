f6ab91b72b88998deafd420561832f6bb050bb5c
0liverkot <oliverkot19@gmail.com>
2025-09-23 09:58:15 +0100

    boilerplate for api service started, need to fill in the user and task services

--- Client/enable/src/CalendarPage.jsx
+++ Client/enable/src/CalendarPage.jsx
@@ -2,13 +2,28 @@ import React from "react";
 import theme from "./theme";
 import Navbar from "./Components/Navbar";
 import { ThemeProvider } from "@emotion/react";
+import { Container, Grid } from "@mui/material";
 
 const CalendarPage = () => {
+
+
 
     return (
         <>
         <ThemeProvider theme={theme}>
         <Navbar currentPage={"calendar"}/>
+            <Container maxWidth={"xxl"}>
+                <Grid container>
+                    <Grid size={3}>
+                        
+                    </Grid>
+                    <Grid size={9}>
+                        <>
+                        calendar
+                        </>
+                    </Grid>
+                </Grid>
+            </Container>
         </ThemeProvider>
         </>
     )
