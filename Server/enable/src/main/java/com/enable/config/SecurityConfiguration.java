e570a4ca5428b1b312da0e949ebb2374690cb7ab
0liverkot <oliverkot19@gmail.com>
2025-09-29 15:31:54 +0100

    added auth endpoints to whitelist

--- Server/enable/src/main/java/com/enable/config/SecurityConfiguration.java
+++ Server/enable/src/main/java/com/enable/config/SecurityConfiguration.java
@@ -23,7 +23,7 @@ public class SecurityConfiguration {
         http.csrf()
             .disable()
             .authorizeHttpRequests()
-            .requestMatchers("") // requests here will be whitelisted
+            .requestMatchers("/api/auth/*") // requests here will be whitelisted
             .permitAll()
             .anyRequest() // all others are authenticated
             .authenticated()
